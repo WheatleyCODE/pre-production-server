3ab8a060b8529c6cec81b548059c2aa9f18f4d5d
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-09-27 10:19:10 +0300

    Delete test, add default app starter, add bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UsersModule,
+  ],
 })
 export class AppModule {}
